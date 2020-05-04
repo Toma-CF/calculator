19308706a221cbcc18d2812e6cad347eab5769a9
Tomi <toma1.cristini@epitech.eu>
2020-05-04 12:00:01 +0200

    Calculator.py : Interface with numbers and corresponding function that receive input

--- calculator.py
+++ calculator.py
@@ -3,11 +3,21 @@
 from tkinter import *
 window = Tk()
 
-def hello():
-	print("hello")
-	pass
 
-Button(window, text="Add smth", command=window.quit).pack()
-Button(window, text="hello", command=hello).pack()
-Button(window, text="Close", command=window.quit).pack()
+def input_key(param):
+	print (param + " has been inputed")
+
+Button(window, text="0", command=lambda: input_key("0")).pack()
+Button(window, text="1", command=lambda: input_key("1")).pack()
+Button(window, text="2", command=lambda: input_key("2")).pack()
+Button(window, text="3", command=lambda: input_key("3")).pack()
+Button(window, text="4", command=lambda: input_key("4")).pack()
+Button(window, text="5", command=lambda: input_key("5")).pack()
+Button(window, text="6", command=lambda: input_key("6")).pack()
+Button(window, text="7", command=lambda: input_key("7")).pack()
+Button(window, text="8", command=lambda: input_key("8")).pack()
+Button(window, text="9", command=lambda: input_key("9")).pack()
+Button(window, text="+", command=lambda: input_key("+")).pack()
+Button(window, text="-", command=lambda: input_key("-")).pack()
+
 window.mainloop()
